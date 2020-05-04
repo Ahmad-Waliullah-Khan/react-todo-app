0b1d1fec7821723c59c063d95448c527c3f01dd5
Ahmad Khan <ahmad.waliullah.khan@gmail.com>
2020-05-04 13:52:51 +0530

    front-end todo app

---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoItem from './TodoItem.js'
+
+class Todos extends React.Component {
+
+  render() {
+    return this.props.todos.map((todo) => (
+      <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo}/>
+      ));
+  }
+
+}
+
+export default Todos;
---
+++ src/components/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>A simple Todo App using React</p>
+    </React.Fragment>
+  )
+}
+
+const headerStyle = {
+  background: '#333',
+  color: '#fff',
+  textAlign: 'center',
+  padding: '10px'
+}
+
+export default About;
